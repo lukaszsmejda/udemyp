0e914db07ec12a089487c54b7b2942efa66886d1
lukaszsmejda <glucio110@interia.pl>
2021-02-17 15:50:37 +0000

    fix: unmatched error

--- webpack/common.js
+++ webpack/common.js
@@ -16,7 +16,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js|jsx)$/,
+                test: /\.js|jsx$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
