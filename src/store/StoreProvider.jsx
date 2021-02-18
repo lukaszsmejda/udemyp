b6def909adcbb23d733b7295580efbc1005447cb
lukaszsmejda <glucio110@interia.pl>
2021-02-18 12:24:49 +0000

    fix: put hooks inside functional component

--- src/store/StoreProvider.jsx
+++ src/store/StoreProvider.jsx
@@ -6,23 +6,23 @@ export const StoreContext = createContext(null);
 
 
 
-const [courses, setCourses] = useState([]);
-const [user, setUser] = useState(null);
-
-
-
-const fetchData = async () => {
-    const { data } = await request.get('/courses');
-    setCourses(data.courses)
-
-
-
-}
-useEffect(() => {
-    fetchData()
-}, [])
 
 const StoreProvider = ({ children }) => {
+    const [courses, setCourses] = useState([]);
+    const [user, setUser] = useState(null);
+
+
+    const fetchData = async () => {
+        const { data } = await request.get('/courses');
+        setCourses(data.courses)
+
+
+
+    }
+    useEffect(() => {
+        fetchData()
+    }, [])
+
 
 
     return (
